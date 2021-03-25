961dfd048b21e7acbd11c0214b718ec38e0cf76e
canguden <80042843+canguden@users.noreply.github.com>
2021-03-25 14:53:21 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// code founded on codepen.io
+
+$('.Navigation--collapsed').click(function() {
+    $('.Overlay').toggleClass('Overlay--on');
+    $('.BurgerMenu').toggleClass('BurgerMenu--on');
+  });
